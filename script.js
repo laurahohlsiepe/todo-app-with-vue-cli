956ca304c86e9a41d3b3abc0b00daa0f4c0b36f6
laurahohlsiepe <l.hohlsiepe@gmail.com>
2022-04-07 09:48:19 +0200

    commit

--- script.js
+++ script.js
@@ -3,11 +3,36 @@ Vue.createApp({
     return {
       todos: [],
       newTodo: [],
+      allTodos: "",
     };
   },
   methods: {
     addTodos() {
       this.todos.push(this.newTodo);
     },
+
+    deleteTodos() {
+      this.todos = this.todos.filter(function (todo) {
+        todo.done;
+      });
+    },
+  },
+
+  computed: {
+    filterOpenTodos() {
+      return this.todos.filter((todo) => {
+        return todo.done === false;
+      });
+    },
   },
 }).mount("#app");
+
+/*
+ deleteTodos() {
+    this.todos = this.todos.filter((todo) => {
+      if (this.todo.checked === true) {
+       return todo;
+      }
+    },
+    )},
+    */
